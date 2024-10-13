d41549c0b57ceeb08482941450ae1f21e5a7c807
James Walker <james.chunho@gmail.com>
2024-10-13 15:54:39 +0800

    move ping to test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use std::{net::Ipv4Addr, time::Duration};
 
 fn main() {
-    ping::ping(
-        std::net::IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8)),
-        Some(Duration::from_secs(2)),
-        None,
-        None,
-        None,
-        None,
-    )
-    .expect("failed to ping");
+    println!("Hello, world!")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ping_8888() {
+        ping::ping(
+            std::net::IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8)),
+            Some(Duration::from_secs(2)),
+            None,
+            None,
+            None,
+            None,
+        )
+        .expect("failed to ping");
+    }
 }
