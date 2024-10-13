3f594fa49e70e1f930c3c4372a62f29a92bb81f0
James Walker <james.chunho@gmail.com>
2024-10-13 16:04:29 +0800

    counter example

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,49 @@
-use std::{net::Ipv4Addr, time::Duration};
+use iced::widget::{button, column, text, Column};
+use iced::Center;
 
-fn main() {
-    println!("Hello, world!")
+#[derive(Default)]
+struct Counter {
+    value: i64,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Increment,
+    Decrement,
+}
+
+impl Counter {
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
+        }
+    }
+
+    fn view(&self) -> Column<Message> {
+        column![
+            button("Increment").on_press(Message::Increment),
+            text(self.value).size(50),
+            button("Decrement").on_press(Message::Decrement)
+        ]
+        .padding(20)
+        .align_x(Center)
+    }
+}
+
+fn main() -> iced::Result {
+    iced::run("A cool counter", Counter::update, Counter::view)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    use std::{net::Ipv4Addr, time::Duration};
 
     #[test]
     fn ping_8888() {
