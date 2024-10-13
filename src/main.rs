23a59922642df18c1049cccbc40eb74949df440d
James Walker <james.chunho@gmail.com>
2024-10-13 15:51:07 +0800

    ping example code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{net::Ipv4Addr, time::Duration};
+
 fn main() {
-    println!("Hello, world!");
+    ping::ping(
+        std::net::IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8)),
+        Some(Duration::from_secs(2)),
+        None,
+        None,
+        None,
+        None,
+    )
+    .expect("failed to ping");
 }
