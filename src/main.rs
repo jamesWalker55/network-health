d56ed18bb36a1f16561d40b4c66acad00f5a3764
James Walker <james.chunho@gmail.com>
2024-10-13 17:01:11 +0800

    implement ping loop code

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,127 @@
-use iced::widget::{button, column, text, Column};
+use blocking::unblock;
+use futures_timer::Delay;
+use iced::futures::future;
+use iced::futures::sink::SinkExt;
+use iced::futures::Stream;
+use iced::stream;
+use iced::widget::{column, text, Column};
 use iced::Center;
+use iced::Subscription;
+use std::{net::Ipv4Addr, time::Duration};
 
-#[derive(Default)]
-struct Counter {
-    value: i64,
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum PingStatus {
+    /// Pinged successfully
+    Success,
+    /// Still pinging, taking a long time
+    Warning,
+    /// Ping failed / timeout
+    Failed,
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    Increment,
-    Decrement,
+struct PingCell {
+    status: PingStatus,
 }
 
-impl Counter {
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::Increment => {
-                self.value += 1;
-            }
-            Message::Decrement => {
-                self.value -= 1;
-            }
+impl Default for PingCell {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl PingCell {
+    fn new() -> Self {
+        Self {
+            status: PingStatus::Success,
         }
     }
 
-    fn view(&self) -> Column<Message> {
-        column![
-            button("Increment").on_press(Message::Increment),
-            text(self.value).size(50),
-            button("Decrement").on_press(Message::Decrement)
-        ]
-        .padding(20)
-        .align_x(Center)
+    fn update(&mut self, new_status: PingStatus) {
+        match new_status {
+            PingStatus::Warning => {
+                if self.status == PingStatus::Success {
+                    self.status = new_status
+                }
+            }
+            new_status => self.status = new_status,
+        }
+    }
+
+    fn view(&self) -> Column<PingStatus> {
+        let x = match self.status {
+            PingStatus::Success => text("Success"),
+            PingStatus::Warning => text("Warning"),
+            PingStatus::Failed => text("Failed"),
+        };
+        column![x.size(50)].padding(20).align_x(Center)
+    }
+
+    fn subscription(&self) -> Subscription<PingStatus> {
+        Subscription::run(Self::subscription_worker)
+    }
+
+    fn subscription_worker() -> impl Stream<Item = PingStatus> {
+        stream::channel(100, |mut output| async move {
+            let _ = output.send(PingStatus::Success).await;
+
+            loop {
+                println!("Start loop!");
+                let loop_duration = Delay::new(Duration::from_secs(2));
+
+                let ping_task = unblock(|| {
+                    println!("  pinging...");
+                    ping::ping(
+                        std::net::IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8)),
+                        Some(Duration::from_secs(2)),
+                        None,
+                        None,
+                        None,
+                        None,
+                    )
+                });
+
+                let _ = match future::select(ping_task, Delay::new(Duration::from_secs(1))).await {
+                    // ping done before 1 second
+                    future::Either::Left((ping_result, _)) => match ping_result {
+                        Ok(_) => {
+                            println!("  ping Success!");
+                            output.send(PingStatus::Success).await
+                        }
+                        Err(_) => {
+                            println!("  ping Failed!");
+                            output.send(PingStatus::Failed).await
+                        }
+                    },
+                    // 1 second passed, ping not done
+                    future::Either::Right((_, ping_task)) => {
+                        println!("  1 second passed...");
+                        let _ = output.send(PingStatus::Warning).await;
+
+                        match ping_task.await {
+                            Ok(_) => {
+                                println!("  ping Success!");
+                                output.send(PingStatus::Success).await
+                            }
+                            Err(_) => {
+                                println!("  ping Failed!");
+                                output.send(PingStatus::Failed).await
+                            }
+                        }
+                    }
+                };
+
+                // don't overload the pinging
+                loop_duration.await;
+            }
+        })
     }
 }
 
 fn main() -> iced::Result {
-    iced::run("A cool counter", Counter::update, Counter::view)
+    // fn main() {
+    iced::application("A cool counter", PingCell::update, PingCell::view)
+        .subscription(PingCell::subscription)
+        .run()
 }
 
 #[cfg(test)]
